dd8c6f9e23bc9de9e19600c86379a30d6aa5c694
pcahYa <puspachyngrm61@gmail.com>
2023-10-09 11:44:50 +0700

    rental buku ci

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+    <tittle>Latihan 1<tittle>
+</heaad>
+
+<body>
+    Halo kawan.. Yuk kita belajar web programming..!!!<br>
+    Nilai 1= <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2. " = " . $hasil; ?>
+
+</body>
